56c2bdc437f15099df12aa33e86cf00e8fda6cd1
Wesley Smith <WesleySmith163@gmail.com>
2023-05-28 01:33:15 -0700

    support displacements

--- InstructionDecode8086/InstructionDecode8086.cpp
+++ InstructionDecode8086/InstructionDecode8086.cpp
@@ -1,8 +1,10 @@
 // InstructionDecode8086.cpp
 
+#include <array>
 #include <cstdint>
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <unordered_map>
 #include <vector>
 
@@ -26,16 +28,21 @@ namespace
         uint8_t mod = 0;
         uint8_t reg = 0;
         uint8_t rm = 0;
+        uint8_t disp_lo = 0;
+        uint8_t disp_hi = 0;
         bool d = false;
         bool w = false;
     };
 
-    constexpr uint8_t mod_regreg = 0b11;
-
-    constexpr char registers[16][3] =
+    constexpr std::array<const char*, 16> registers =
     {
         "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh",
-        "ax", "cx", "dx", "bx", "sp", "bp", "si", "di",
+        "ax", "cx", "dx", "bx", "sp", "bp", "si", "di"
+    };
+
+    constexpr std::array<const char*, 8> effective_addresses =
+    {
+        "bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"
     };
 
     constexpr const char* mov_name = "mov";
@@ -108,23 +115,48 @@ int main()
         switch (opcode)
         {
         case opcode::mov_normal:
+        {
             inst.w = b & 1;
             b >>= 1;
             inst.d = b & 1;
             b >>= 1;
             inst.opcode = opcode;
 
-            if (input_file >> b)
-            {
-                inst.rm = b & 0b0111;
-                b >>= 3;
-                inst.reg = b & 0b0111;
-                b >>= 3;
-                inst.mod = b;
+            input_file >> b;
 
-                instructions.push_back(inst);
+            inst.rm = b & 0b0111;
+            b >>= 3;
+            inst.reg = b & 0b0111;
+            b >>= 3;
+            inst.mod = b;
+
+            int displacement_bytes = 0;
+            switch (inst.mod)
+            {
+            case 0b00: // memory mode, no displacement unless direct address
+                displacement_bytes = (inst.rm == 0b110) * 2;
+                break;
+            case 0b01: // memory mode, 8-bit displacement
+                displacement_bytes = 1;
+                break;
+            case 0b10: // memory mode, 16-bit displacement
+                displacement_bytes = 2;
+                break;
+            case 0b11: // register mode, no displacement
+                break;
             }
+
+            // read displacement
+            if (displacement_bytes > 0)
+            {
+                input_file >> inst.disp_lo;
+
+                if (displacement_bytes > 1)
+                    input_file >> inst.disp_hi;
+            }
+
             break;
+        }
 
         case opcode::mov_immediate_to_register_or_memory:
             break;
@@ -143,21 +175,74 @@ int main()
         case opcode::none:
             break;
         }
+
+        instructions.push_back(inst);
     }
 
     // decode instruction and print asm
     for (auto& inst : instructions)
     {
-        if (inst.mod != mod_regreg)
-            continue; // only supporting reg-to-reg
-
-        const uint8_t source = inst.d ? inst.rm : inst.reg;
-        const uint8_t destination = inst.d ? inst.reg : inst.rm;
-
-        const char* source_name = registers[source + 8 * inst.w];
-        const char* destination_name = registers[destination + 8 * inst.w];
-
         const char* opcode = opcodes[inst.opcode];
+
+        std::string source_name;
+        std::string destination_name;
+
+        switch (inst.opcode)
+        {
+        case opcode::mov_normal:
+            {
+                if (inst.mod == 0b11)
+                {
+                    // register mode
+                    if (inst.d)
+                    {
+                        source_name = registers[inst.rm + 8 * inst.w];
+                        destination_name = registers[inst.reg + 8 * inst.w];
+                    }
+                    else
+                    {
+                        source_name = registers[inst.reg + 8 * inst.w];
+                        destination_name = registers[inst.rm + 8 * inst.w];
+                    }
+                }
+                else
+                {
+                    std::string address = effective_addresses[inst.rm];
+
+                    // memory mode, 8-bit displacement
+                    if (inst.mod == 0b01)
+                        address += " + " + std::to_string(inst.disp_lo);
+
+                    // memory mode, 16-bit displacement
+                    if (inst.mod == 0b10)
+                        address += " + " + std::to_string((inst.disp_hi << 8) + inst.disp_lo);
+
+                    if (inst.mod == 0b00 && inst.rm == 0b110)
+                    {
+                        // direct address
+                        // todo
+                    }
+
+                    if (inst.d)
+                    {
+                        source_name = "[" + address + "]";
+                        destination_name = registers[inst.reg + 8 * inst.w];
+                    }
+                    else
+                    {
+                        source_name = registers[inst.reg + 8 * inst.w];
+                        destination_name = "[" + address + "]";
+                    }
+                }
+            }
+        case opcode::mov_immediate_to_register_or_memory: break;
+        case opcode::mov_immediate_to_register: break;
+        case opcode::mov_memory_to_accumulator: break;
+        case opcode::mov_accumulator_to_memory: break;
+        case opcode::mov_to_segment_register: break;
+        case opcode::mov_from_segment_register: break;
+        case opcode::none: break;
+        }
 
         std::cout << opcode << ' ' << destination_name << ", " << source_name << '\n';
     }
