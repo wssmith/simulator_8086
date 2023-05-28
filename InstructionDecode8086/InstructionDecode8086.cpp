620b6f351da4e8ccaf01edf87b40c7fbf593baa4
Wesley Smith <WesleySmith163@gmail.com>
2023-05-27 23:44:05 -0700

    account for different types of mov instructions

--- InstructionDecode8086/InstructionDecode8086.cpp
+++ InstructionDecode8086/InstructionDecode8086.cpp
@@ -6,30 +6,89 @@
 #include <unordered_map>
 #include <vector>
 
-struct instruction
+namespace
 {
-    uint8_t opcode = 0;
-    uint8_t mod = 0;
-    uint8_t reg = 0;
-    uint8_t rm = 0;
-    bool d = false;
-    bool w = false;
-};
+    enum class opcode : uint16_t
+    {
+        none,
+        mov_normal,
+        mov_immediate_to_register_or_memory,
+        mov_immediate_to_register,
+        mov_memory_to_accumulator,
+        mov_accumulator_to_memory,
+        mov_to_segment_register,
+        mov_from_segment_register
+    };
 
-constexpr uint8_t mod_regreg = 0b11;
+    struct instruction
+    {
+        opcode opcode = opcode::none;
+        uint8_t mod = 0;
+        uint8_t reg = 0;
+        uint8_t rm = 0;
+        bool d = false;
+        bool w = false;
+    };
 
-constexpr char registers[16][3] =
-{
-    "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh",
-    "ax", "cx", "dx", "bx", "sp", "bp", "si", "di",
-};
+    constexpr uint8_t mod_regreg = 0b11;
 
-constexpr uint8_t mov_op = 0b10'0010;
+    constexpr char registers[16][3] =
+    {
+        "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh",
+        "ax", "cx", "dx", "bx", "sp", "bp", "si", "di",
+    };
 
-std::unordered_map<uint8_t, const char*> opcodes
-{
-    { mov_op, "mov" }
-};
+    constexpr const char* mov_name = "mov";
+
+    std::unordered_map<opcode, const char*> opcodes
+    {
+        { opcode::mov_normal, mov_name },
+        { opcode::mov_immediate_to_register_or_memory, mov_name },
+        { opcode::mov_immediate_to_register, mov_name },
+        { opcode::mov_memory_to_accumulator, mov_name },
+        { opcode::mov_accumulator_to_memory, mov_name },
+        { opcode::mov_to_segment_register, mov_name },
+        { opcode::mov_from_segment_register, mov_name },
+    };
+
+    opcode determine_opcode(uint8_t b)
+    {
+        switch (b >> 4)
+        {
+        case 0b1000:
+        {
+            const uint8_t second_half = b & 0b1111;
+            switch (second_half)
+            {
+            case 0b1110:
+                return opcode::mov_to_segment_register;
+            case 0b1100:
+                return opcode::mov_from_segment_register;
+            default:
+                if ((second_half & 0b1100) == 0b1000)
+                    return opcode::mov_normal;
+                break;
+            }
+            break;
+        }
+        case 0b1100:
+            return opcode::mov_immediate_to_register_or_memory;
+        case 0b1011:
+            return opcode::mov_immediate_to_register;
+        case 0b1010:
+            switch (b & 0b1110)
+            {
+            case 0b0000:
+                return opcode::mov_memory_to_accumulator;
+            case 0b0010:
+                return opcode::mov_accumulator_to_memory;
+            }
+            break;
+        }
+
+        return opcode::none;
+    }
+}
 
 int main()
 {
@@ -42,26 +101,47 @@ int main()
     uint8_t b;
     while (input_file >> b)
     {
-        instruction inst;
+        instruction inst{};
 
-        inst.w = b & 1;
-        b >>= 1;
-        inst.d = b & 1;
-        b >>= 1;
-        inst.opcode = b;
+        const opcode opcode = determine_opcode(b);
 
-        if (input_file >> b)
+        switch (opcode)
         {
-            if (inst.opcode != mov_op)
-                continue; // only supporting mov for now 
+        case opcode::mov_normal:
+            inst.w = b & 1;
+            b >>= 1;
+            inst.d = b & 1;
+            b >>= 1;
+            inst.opcode = opcode;
 
-            inst.rm = b & 0b0111;
-            b >>= 3;
-            inst.reg = b & 0b0111;
-            b >>= 3;
-            inst.mod = b;
+            if (input_file >> b)
+            {
+                inst.rm = b & 0b0111;
+                b >>= 3;
+                inst.reg = b & 0b0111;
+                b >>= 3;
+                inst.mod = b;
 
-            instructions.push_back(inst);
+                instructions.push_back(inst);
+            }
+            break;
+
+        case opcode::mov_immediate_to_register_or_memory:
+            break;
+
+        case opcode::mov_immediate_to_register:
+            break;
+
+        case opcode::mov_memory_to_accumulator:
+        case opcode::mov_accumulator_to_memory:
+            break;
+
+        case opcode::mov_to_segment_register:
+        case opcode::mov_from_segment_register:
+            break;
+
+        case opcode::none:
+            break;
         }
     }
 
